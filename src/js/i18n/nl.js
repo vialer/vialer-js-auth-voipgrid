a74c033e6e3734ff6b2648a86881a7e32ca8303f
Jeroen van Veen <jeroen@wearespindle.com>
2018-10-30 14:32:40 +0100

    fix: add missing dutch translation.

--- src/js/i18n/nl.js
+++ src/js/i18n/nl.js
@@ -2,6 +2,7 @@ module.exports = {
     'a partner user cannot be used to login with.': 'een partner gebruiker kan niet gebruikt worden.',
     'invalid two factor token. Please check your tokenizer.': 'ongeldige two-factor token. Controleer je tokenizer.',
     'more info': 'meer info',
+    'please change your {name} password first.': 'verander eerst je {name} wachtwoord.',
     'too many failed login attempts - wait until {date}': 'teveel mislukte inlogpogingen (wacht tot {date})',
     'unexpected error': 'onverwachte fout'
 }
